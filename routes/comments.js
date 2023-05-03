a23576a305bacb69eef24b26297898a68b738206
Dongseon <elwynn94@gmail.com>
2023-05-03 23:29:39 +0900

    Update comments.js

--- routes/comments.js
+++ routes/comments.js
@@ -18,7 +18,7 @@ router.post('/comments/:postId', async (req, res) => {
 })
 
 // 댓글 목록 조회 API - 조회하는 게시글에 작성된 모든 댓글을 목록 형식으로 / 작성 날짜 기준으로 내림차순 정렬 (최신순)
-router.get('/comments', async (req, res) => {
+router.get('/comments/:postId', async (req, res) => {
     const {postId} = req.params;
     const comments = await Comments
                     .find({postId: postId},{_id: 1, userName: 1, content: 1, createdAt: 1})
